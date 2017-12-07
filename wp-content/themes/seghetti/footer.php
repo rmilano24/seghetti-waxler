9f6a8192f86a4dfbe9e1107cbaffa2d4afc7ae6a
rmilano24 <rmilano24@gmail.com>
2017-12-06 22:26:35 -0800

    set up dev enviorment

---
+++ wp-content/themes/seghetti/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "off-canvas-wrap" div and all content after.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+?>
+
+<div class="footer-container">
+	<footer class="footer">
+		<?php dynamic_sidebar( 'footer-widgets' ); ?>
+	</footer>
+</div>
+
+<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+	</div><!-- Close off-canvas content -->
+<?php endif; ?>
+
+<?php wp_footer(); ?>
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
+//]]></script>
+</body>
+</html>
