5fbf8882c39a068f3c5c48ceddf1b1b0aad5134c
rmilano24 <rmilano24@gmail.com>
2017-12-13 15:30:00 -0800

    add new images

---
+++ wp-content/themes/seghetti/page-templates/contact.php
@@ -0,0 +1,158 @@
+<?php
+/*
+Template Name: Contact
+*/
+get_header(); ?>
+
+<header class="front-hero" role="banner">
+
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-7">
+				<div class="hero-text">
+					<span class="small-intro">What we do</span>
+					<h1>Take the edge<br>
+					<b>Off your taxes</b></h1>
+					<div class="seperator"></div>
+					<p class="large">There are few <thf></thf>ings that keep us awake at night like our taxes. Let’s face it, whether you’re doing your taxes by yourself or paying someone to prepare them for you, it can be a miserable experience. At Seghetti Waxler, we aim to make that experience better.</p>
+					<a href="#" class="button">Contact Us</a><a href="#" class="button ghost">Learn More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</header>
+
+
+<section class="intro">
+
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-6">
+				<span class="small-intro">A different kind of tax experience</span>
+				<h2>Our industry has forgotten about the client.</h2>
+				<div class="seperator"></div>
+				<p class="large">There are few things that keep us awake at night like our taxes. Let’s face it, whether you’re doing your taxes by yourself or paying someone to prepare them for you, it can be a miserable experience. At Seghetti Waxler, we aim to make that experience better.</p>
+			</div>
+			<div class="cell medium-6">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/seghetti-team@2x.jpg" alt="Seghetti Waxler Team" />
+			</div>
+		</div>
+	</div>
+
+	<div class="grid-container" id="reasons">
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-4">
+
+				<div class="grid-x grid-padding-x">
+					<div class="cell medium-4">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/approachable@2x.jpg" alt="Seghetti Waxler" />
+					</div>
+					<div class="cell medium-8">
+						<div class="number">01<span class="yellow">_</span></div>
+						<h3>Approachable</h3>
+						<p>Seghetti Waxler’s CPAs and staff are friendly and speak in plain language. The tax code is complex. You need a firm that you feel comfortable working with to ensure your issues are understood.</p>
+					</div>
+				</div>
+
+			</div>
+			<div class="cell medium-4">
+
+				<div class="grid-x grid-padding-x">
+					<div class="cell medium-4">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/approachable@2x.jpg" alt="Seghetti Waxler" />
+					</div>
+					<div class="cell medium-8">
+						<div class="number">02<span class="yellow">_</span></div>
+						<h3>Approachable</h3>
+						<p>Seghetti Waxler’s CPAs and staff are friendly and speak in plain language. The tax code is complex. You need a firm that you feel comfortable working with to ensure your issues are understood.</p>
+					</div>
+				</div>
+
+			</div>
+			<div class="cell medium-4">
+
+				<div class="grid-x grid-padding-x">
+					<div class="cell medium-4">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/approachable@2x.jpg" alt="Seghetti Waxler" />
+					</div>
+					<div class="cell medium-8">
+						<div class="number">01<span class="yellow">_</span></div>
+						<h3>Approachable</h3>
+						<p>Seghetti Waxler’s CPAs and staff are friendly and speak in plain language. The tax code is complex. You need a firm that you feel comfortable working with to ensure your issues are understood.</p>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+</section>
+
+<section class="serives bg_blue">
+
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-3">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/business-valutions@2x.jpg" alt="Seghetti Waxler" />
+					<h4>Tax Preparation</h4>
+					<div class="seperator"></div>
+					<p>Let Seghetti Waxler’s CPAs and staff ease the burden of your annual tax filings.</p>
+					<a href="#">Learn More</a>
+			</div>
+			<div class="cell medium-3">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/business-valutions@2x.jpg" alt="Seghetti Waxler" />
+				<h4>Tax Preparation</h4>
+				<div class="seperator"></div>
+				<p>Let Seghetti Waxler’s CPAs and staff ease the burden of your annual tax filings.</p>
+				<a href="#">Learn More</a>
+			</div>
+			<div class="cell medium-3">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/business-valutions@2x.jpg" alt="Seghetti Waxler" />
+				<h4>Tax Preparation</h4>
+				<div class="seperator"></div>
+				<p>Let Seghetti Waxler’s CPAs and staff ease the burden of your annual tax filings.</p>
+				<a href="#">Learn More</a>
+			</div>
+			<div class="cell medium-3">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/business-valutions@2x.jpg" alt="Seghetti Waxler" />
+				<h4>Tax Preparation</h4>
+				<div class="seperator"></div>
+				<p>Let Seghetti Waxler’s CPAs and staff ease the burden of your annual tax filings.</p>
+				<a href="#">Learn More</a>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<section class="testimonials">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-12">
+				<blockquote>
+					As a professional athlete and model, my tax and financial situation can get very complicated. I rely on Seghetti Waxler to help with my strategic tax planning and to stay out of trouble with the IRS.
+				</blockquote>
+				<div class="person"><span class="yellow">—</span> Luke Rockhold, <b>UFC® Fighter, Model</b></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="meet-the-team">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-4">
+				<span class="small-intro">Get to know us</span>
+				<h2>Meet our team</h2>
+				<div class="seperator"></div>
+				<p>Let Seghetti Waxler’s CPAs and staff ease the burden of your annual tax filings.</p>
+				<a href="#">meet the team</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php get_footer();
