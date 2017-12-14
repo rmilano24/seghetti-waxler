3638e729e530e2d44ec7372fc35191024efa85a5
rmilano24 <rmilano24@gmail.com>
2017-12-13 16:49:57 -0800

    add contact page layout

--- wp-content/themes/seghetti/page-templates/about.php
+++ wp-content/themes/seghetti/page-templates/about.php
@@ -4,25 +4,22 @@ Template Name: About
 */
 get_header(); ?>
 
-<header class="front-hero" role="banner">
+<header class="inner-hero" role="banner">
 
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-7">
 				<div class="hero-text">
 					<span class="small-intro">What we do</span>
-					<h1>Take the edge<br>
-					<b>Off your taxes</b></h1>
+					<h1>About Us</h1>
 					<div class="seperator"></div>
-					<p class="large">There are few <thf></thf>ings that keep us awake at night like our taxes. Let’s face it, whether you’re doing your taxes by yourself or paying someone to prepare them for you, it can be a miserable experience. At Seghetti Waxler, we aim to make that experience better.</p>
-					<a href="#" class="button">Contact Us</a><a href="#" class="button ghost">Learn More</a>
+					<p class="large">We help take the edge off your taxes</p>
 				</div>
 			</div>
 		</div>
 	</div>
 
 </header>
-
 
 <section class="intro">
 
@@ -90,7 +87,7 @@ get_header(); ?>
 
 </section>
 
-<section class="serives bg_blue">
+<section id="meet-the-team" class="bg_blue">
 
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x">
@@ -127,32 +124,14 @@ get_header(); ?>
 
 </section>
 
-<section class="testimonials">
+<section class="cta">
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-12">
-				<blockquote>
-					As a professional athlete and model, my tax and financial situation can get very complicated. I rely on Seghetti Waxler to help with my strategic tax planning and to stay out of trouble with the IRS.
-				</blockquote>
-				<div class="person"><span class="yellow">—</span> Luke Rockhold, <b>UFC® Fighter, Model</b></div>
+				Ready to take the edge off?
 			</div>
 		</div>
 	</div>
 </section>
-
-<section class="meet-the-team">
-	<div class="grid-container">
-		<div class="grid-x grid-padding-x">
-			<div class="cell medium-4">
-				<span class="small-intro">Get to know us</span>
-				<h2>Meet our team</h2>
-				<div class="seperator"></div>
-				<p>Let Seghetti Waxler’s CPAs and staff ease the burden of your annual tax filings.</p>
-				<a href="#">meet the team</a>
-			</div>
-		</div>
-	</div>
-</section>
-
 
 <?php get_footer();
