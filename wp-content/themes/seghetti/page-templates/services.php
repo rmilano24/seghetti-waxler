1e5a4f1a08ed05e207763229b500fbdd4cd2d8c3
rmilano24 <rmilano24@gmail.com>
2017-12-18 21:34:13 -0800

    loop in services

--- wp-content/themes/seghetti/page-templates/services.php
+++ wp-content/themes/seghetti/page-templates/services.php
@@ -10,10 +10,10 @@ get_header(); ?>
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-7">
 				<div class="hero-text">
-					<span class="small-intro">What we do</span>
-					<h1>Services</h1>
+					<span class="small-intro"><?php the_field('hero_small_text'); ?></span>
+					<h1><?php the_field('hero_header'); ?></h1>
 					<div class="seperator"></div>
-					<p class="large">Here’s what we can do for you</p>
+					<p class="large"><?php the_field('hero_content'); ?></p>
 				</div>
 			</div>
 		</div>
@@ -23,58 +23,40 @@ get_header(); ?>
 
 
 <section class="services">
+	<?php
+			 $args = array(
+				 'post_type' => 'capabilities',
+				 'posts_per_page' => 8
+			 );
+			 $my_capabilities = new WP_Query( $args );
+			 if( $my_capabilities->have_posts() ) {
+				 while( $my_capabilities ->have_posts() ) {
+					 $my_capabilities->the_post();
+			?>
 
-		<div class="grid-x grid-padding-x">
-			<div class="cell medium-6 service-content">
-				<span class="small-intro">A different kind of tax experience</span>
-				<h2>Our industry has forgotten about the client.</h2>
-				<div class="seperator"></div>
-				<p class="large">There are few things that keep us awake at night like our taxes. Let’s face it, whether you’re doing your taxes by yourself or paying someone to prepare them for you, it can be a miserable experience. At Seghetti Waxler, we aim to make that experience better.</p>
-				<a href="#" class="button">Learn More</a>
-			</div>
-			<div class="cell medium-6 bg_service-image" id="tax-prep">
+			<div class="grid-x grid-padding-x">
+				<div class="cell medium-6 service-content">
+					<span class="small-intro"><?php the_field('small_header_text'); ?></span>
+					<h2><?php the_title(); ?></h2>
+					<div class="seperator"></div>
+					<p class="large"><?php the_field('intro_text'); ?></p>
+					<a href="<?php the_field('link_to_service_page'); ?>" class="button">Learn More</a>
+				</div>
+				<div class="cell medium-6 bg_service-image" id="tax-prep">
 
+				</div>
 			</div>
-		</div>
-
-
-		<div class="grid-x grid-padding-x">
-			<div class="cell medium-6 bg_service-image" id="tax-prep">
-			</div>
-			<div class="cell medium-6 service-content">
-				<span class="small-intro">A different kind of tax experience</span>
-				<h2>Our industry has forgotten about the client.</h2>
-				<div class="seperator"></div>
-				<p class="large">There are few things that keep us awake at night like our taxes. Let’s face it, whether you’re doing your taxes by yourself or paying someone to prepare them for you, it can be a miserable experience. At Seghetti Waxler, we aim to make that experience better.</p>
-				<a href="#" class="button">Learn More</a>
-			</div>
-		</div>
-
-		<div class="grid-x grid-padding-x">
-			<div class="cell medium-6 service-content">
-				<span class="small-intro">A different kind of tax experience</span>
-				<h2>Our industry has forgotten about the client.</h2>
-				<div class="seperator"></div>
-				<p class="large">There are few things that keep us awake at night like our taxes. Let’s face it, whether you’re doing your taxes by yourself or paying someone to prepare them for you, it can be a miserable experience. At Seghetti Waxler, we aim to make that experience better.</p>
-				<a href="#" class="button">Learn More</a>
-			</div>
-			<div class="cell medium-6 bg_service-image" id="tax-prep">
-
-			</div>
-		</div>
 
 
-		<div class="grid-x grid-padding-x">
-			<div class="cell medium-6 bg_service-image" id="tax-prep">
-			</div>
-			<div class="cell medium-6 service-content">
-				<span class="small-intro">A different kind of tax experience</span>
-				<h2>Our industry has forgotten about the client.</h2>
-				<div class="seperator"></div>
-				<p class="large">There are few things that keep us awake at night like our taxes. Let’s face it, whether you’re doing your taxes by yourself or paying someone to prepare them for you, it can be a miserable experience. At Seghetti Waxler, we aim to make that experience better.</p>
-				<a href="#" class="button">Learn More</a>
-			</div>
-		</div>
+			<?php
+					}
+				}
+				else {
+					echo 'There are currently no capabilities added.';
+				}
+			?>
+		 <?php wp_reset_query(); ?>
+
 
 </section>
 
